c66ec108f48c3f482cad1e29d6139b9734c70c95
Foxcorn Lab <43523802+foxcornlab@users.noreply.github.com>
2019-07-19 20:09:32 +0530

    Update and rename .constants.js.example to .constants.js

---
+++ src/.constants.js
@@ -0,0 +1,22 @@
+module.exports = Object.freeze({
+  TOKEN: '733462745:AAHm9FIBR-Qd_M5PnvnkEPbMA5lxlLE_MSI',
+  ARIA_SECRET: 'password',
+  ARIA_DOWNLOAD_LOCATION: '/home/greatindianparody/aria-telegram-mirror-bot/downloads',
+  ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
+  ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings
+  ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded
+  GDRIVE_PARENT_DIR_ID: '1cpFPijI6AnRQNWsakt1NUYw1oniASICT',
+  SUDO_USERS: [658490863],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [346535810],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
+  DRIVE_FILE_PRIVATE: {
+    ENABLED: false,
+    EMAILS: ['someMail@gmail.com', 'someOtherMail@gmail.com']
+  },
+  DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
+    enabled: false,   // Set this to true to use the notify functionality
+    host: 'hostname.domain',
+    port: 80,
+    path: '/botNotify'
+  } 
+});
